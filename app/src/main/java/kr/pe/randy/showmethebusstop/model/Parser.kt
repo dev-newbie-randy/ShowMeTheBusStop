0925f43b8f33eedc233810b2637689e78b89ac10
Randy Sangyoon Shin <ssy79@infrawareglobal.com>
2020-01-28 11:48:50 +0900

    Parser를 class에서 object로 변경

--- app/src/main/java/kr/pe/randy/showmethebusstop/model/Parser.kt
+++ app/src/main/java/kr/pe/randy/showmethebusstop/model/Parser.kt
@@ -3,58 +3,56 @@ package kr.pe.randy.showmethebusstop.model
 import org.json.JSONArray
 import org.json.JSONObject
 
-class Parser {
-    companion object {
-        fun getBusRouteList(busRouteListJson: JSONArray): MutableList<BusRouteData> {
-            val busRouteList = mutableListOf<BusRouteData>()
+object Parser {
+    fun getBusRouteList(busRouteListJson: JSONArray): MutableList<BusRouteData> {
+        val busRouteList = mutableListOf<BusRouteData>()
 
-            val length = busRouteListJson.length() - 1
+        val length = busRouteListJson.length() - 1
 
-            for (i in 0..length) {
-                val busRouteJson = busRouteListJson.getJSONObject(i)
+        for (i in 0..length) {
+            val busRouteJson = busRouteListJson.getJSONObject(i)
 
-                val busRouteData =
-                    BusRouteData(
-                        busRouteJson.optInt("staOrder"),
-                        busRouteJson.optString("routeId"),
-                        busRouteJson.optInt("districtCd"),
-                        busRouteJson.optString("regionName"),
-                        busRouteJson.optString("routeTypeName"),
-                        busRouteJson.optString("routeTypeCd"),
-                        busRouteJson.optString("routeName")
-                    )
-
-                busRouteList.add(busRouteData)
-            }
-            return busRouteList
-        }
-
-        fun getBusRouteList(busRouteJson: JSONObject): MutableList<BusRouteData> {
-            val busRouteList = mutableListOf<BusRouteData>()
-
-            val busRouteData = BusRouteData(
-                busRouteJson.optInt("staOrder"),
-                busRouteJson.optString("routeId"),
-                busRouteJson.optInt("districtCd"),
-                busRouteJson.optString("regionName"),
-                busRouteJson.optString("routeTypeName"),
-                busRouteJson.optString("routeTypeCd"),
-                busRouteJson.optString("routeName")
-            )
+            val busRouteData =
+                BusRouteData(
+                    busRouteJson.optInt("staOrder"),
+                    busRouteJson.optString("routeId"),
+                    busRouteJson.optInt("districtCd"),
+                    busRouteJson.optString("regionName"),
+                    busRouteJson.optString("routeTypeName"),
+                    busRouteJson.optString("routeTypeCd"),
+                    busRouteJson.optString("routeName")
+                )
 
             busRouteList.add(busRouteData)
-            return busRouteList
         }
+        return busRouteList
+    }
 
-        fun convertId(rawId: String): String {
-            val id = rawId.trim()
-            if (id.isNotEmpty()) {
-                val builder = StringBuilder(id.substring(0, 2))
-                builder.append("-")
-                builder.append(id.substring(2, id.length))
-                return builder.toString()
-            }
-            return id
+    fun getBusRouteList(busRouteJson: JSONObject): MutableList<BusRouteData> {
+        val busRouteList = mutableListOf<BusRouteData>()
+
+        val busRouteData = BusRouteData(
+            busRouteJson.optInt("staOrder"),
+            busRouteJson.optString("routeId"),
+            busRouteJson.optInt("districtCd"),
+            busRouteJson.optString("regionName"),
+            busRouteJson.optString("routeTypeName"),
+            busRouteJson.optString("routeTypeCd"),
+            busRouteJson.optString("routeName")
+        )
+
+        busRouteList.add(busRouteData)
+        return busRouteList
+    }
+
+    fun convertId(rawId: String): String {
+        val id = rawId.trim()
+        if (id.isNotEmpty()) {
+            val builder = StringBuilder(id.substring(0, 2))
+            builder.append("-")
+            builder.append(id.substring(2, id.length))
+            return builder.toString()
         }
+        return id
     }
 }
