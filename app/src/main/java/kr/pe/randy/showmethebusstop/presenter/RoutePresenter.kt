9a901e7953c6e73f5b1f4d54b68569b6545be88f
Randy <calpis79@gmail.com>
2020-02-04 23:02:36 +0900

    컨플릭트 수정 테스트 2

--- app/src/main/java/kr/pe/randy/showmethebusstop/presenter/RoutePresenter.kt
+++ app/src/main/java/kr/pe/randy/showmethebusstop/presenter/RoutePresenter.kt
@@ -13,7 +13,6 @@ class RoutePresenter : RouteContract.Presenter, RouteContract.Listener {
     override fun getRouteList(keyword: String) {
         searchView?.getContextIfAvailable()?.let {
             RouteApi.searchBusRoute(keyword, it, this@RoutePresenter)
-            // 컨플릭트 수정
         }
     }
 
